05b8e8f0d4f8b6a0a2a9df2541cd6842f9d52eac
sachinrawat2110 <sachinrawatt00@gmail.com>
2025-03-11 15:08:42 +0530

    Update Subcatroutes.js

--- routes/Subcatroutes.js
+++ routes/Subcatroutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const SubCatcontroller = require('../controllers/SubCatcontroller');
 const upload = require('./multer');
 
-router.post("/savesubcategory",upload.single('cpic'),SubCatcontroller.AddSubcategory)
+router.post("/savesubcategory",upload.single('scpic'),SubCatcontroller.AddSubcategory)
 router.get("/getsubcatbycatid/:cid",SubCatcontroller.GetSubcategory)
-router.put("/updatesubcat",upload.single('cpic'),SubCatcontroller.UpdateSubcat)
+router.put("/updatesubcat",upload.single('scpic'),SubCatcontroller.UpdateSubcat)
 router.delete("/delsubcat/:scid",SubCatcontroller.DeleteSubcat)
 
 module.exports = router;
